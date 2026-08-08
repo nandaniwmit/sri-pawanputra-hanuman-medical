74cf93143ccadc8dcfb85444f4064d37fac0d6ac
nandaniwmit <nandani.wmit@gmail.com>
2026-08-08 10:08:16 +0530

    Update tracking endpoint URL in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export default function App() {
 
   // GLOBAL TRACKER INTEGRATION
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
     if (urlParams.get('cid')) {
